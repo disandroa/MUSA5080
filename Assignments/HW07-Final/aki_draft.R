196889c78be6bbd8625d7cca260b66e584cc02c2
akidi <akira.disandro@pennmedicine.upenn.edu>
2023-12-01 12:31:16 -0500

    adding permit data

--- Assignments/HW07-Final/aki_draft.R
+++ Assignments/HW07-Final/aki_draft.R
@@ -25,52 +25,18 @@ setwd("~/Documents/MUSA5080")
 
 # load data
 {
-  # code from Nissim to download Philadelphia permit data
-  base_url <- "https://phl.carto.com/api/v2/sql"
+  # reading geojson file
+  dat_permit <- st_read("https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+permits&filename=permits&format=geojson&skipfields=cartodb_id")
   
-  three_years_ago <- (lubridate::ymd(Sys.Date()) - lubridate::years(11))
-  one_year_ago <- (lubridate::ymd(Sys.Date()) - lubridate::years(1))
+  st_write(dat_permit,"Assignments/HW07-Final/data/permit_data.geojson")
   
-  building_perms_query <- sprintf("
-                  SELECT 
-                  address,
-                  addressobjectid,
-                  approvedscopeofwork,
-                  commercialorresidential,
-                  opa_account_num,
-                  permittype,
-                  status,
-                  unit_num,
-                  unit_type,
-                  permitissuedate, 
-                  typeofwork, 
-                  ST_Y(the_geom) AS lat, ST_X(the_geom) AS lng
-                  FROM permits
-                  WHERE permitissuedate >= '%s' AND permitissuedate < '%s'
-                 ", three_years_ago, one_year_ago)
-  
-  building_permits <- st_as_sf(get_carto(building_perms_query,
-                                         format = 'csv',
-                                         base_url = base_url,
-                                         stringsAsFactors = FALSE) |>
-                                 dplyr::filter(
-                                   !is.na(lat),
-                                   !is.na(lng)),
-                               coords = c("lng", "lat"),
-                               crs = st_crs('EPSG:4326')) |>
-    st_transform(crs = st_crs("EPSG:2272")) %>%
-    mutate(permits_count = 1)
-  
-  # write permits to the data subfolder
-  building_permits_path <- "data/building_permits.geojson"
-  saveRDS(building_permits, building_permits_path)
 }
 
 # Data Exploration
 {
   # making simple tables of types of permits
-  permit_type <- sort(unique(building_permits$permittype))
-  work_type <- sort(unique(building_permits$typeofwork))
+  permit_type <- sort(unique(dat_permit$permittype))
+  work_type <- sort(unique(dat_permit$typeofwork))
   
   
 }
