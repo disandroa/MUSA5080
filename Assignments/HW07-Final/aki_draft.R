8c0b2865104f372e60f41207e0975300cace4709
akidi <akira.disandro@pennmedicine.upenn.edu>
2023-12-04 20:34:54 -0500

    added more data

--- Assignments/HW07-Final/aki_draft.R
+++ Assignments/HW07-Final/aki_draft.R
@@ -19,51 +19,96 @@ setwd("~/Documents/MUSA5080")
 
 # load functions ----
 {
-  
+  source("https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-Landing/master/functions.r")
 }
 
 
 # load data ----
 {
-  ## philly permit data ----
-  {
-    dat_permit <- st_read("https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+permits&filename=permits&format=geojson&skipfields=cartodb_id")
-    
-    # only keep new construction permits
-    newcon_permits <- dat_permit %>% 
-      filter(grepl("NEW CON|NEWCON",typeofwork))
-  }
+  # philly permit data
+  # dat_permit <- st_read("https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+permits&filename=permits&format=geojson&skipfields=cartodb_id")
+  # 
+  # # only keep new construction permits
+  # newcon_permits <- dat_permit %>% 
+  #   filter(grepl("NEW CON|NEWCON",typeofwork)) %>% 
+  # st_transform('ESRI:102728')
   
+  # write out smaller geojson file
+  # st_write(newcon_permits,"Assignments/HW07-Final/data/newcon_permits.geojson")
   
+  # rm(dat_permit)
   
+  newcon_permits <- st_read("Assignments/HW07-Final/data/newcon_permits.geojson")
   
   # OPA housing permit data
-  
+  {
+    # missing
+  }
   
   # census data
-  census_vars <- c() # census variables of interest
+  census_vars <- c() # census variables of interest 
   
   # philly neighborhood data
   nhoods_path <- 'https://raw.githubusercontent.com/azavea/geo-data/master/Neighborhoods_Philadelphia/Neighborhoods_Philadelphia.geojson'
   nhoods <- st_read(nhoods_path, quiet = T) %>%
     st_transform('ESRI:102728') %>%
     dplyr::select(mapname)
+  
+  # philly bounds
+  philly <- st_read("https://opendata.arcgis.com/datasets/405ec3da942d4e20869d4e1449a2be48_0.geojson") %>%
+    st_transform('ESRI:102728') %>% 
+    dplyr::select(OBJECTID,geometry)
+  
+  # vacant lots/buildings
+  vacant_centroids <- st_read("https://opendata.arcgis.com/datasets/b990222a527849229b4192feb4c42dc0_0.geojson") %>% 
+    st_transform('ESRI:102728') %>% 
+    st_centroid() %>%
+    mutate(Legend = "Vacants") %>%
+    dplyr::select(Legend)
+  
+  # parks & rec
+  ppr_sites <- st_read("https://opendata.arcgis.com/api/v3/datasets/9eb26a787a6e448ba426eea7f9f0d93a_0/downloads/data?format=geojson&spatialRefId=4326") %>% 
+    st_transform('ESRI:102728') %>% 
+    mutate(Legend = "Parks and Rec") %>%
+    dplyr::select(Legend)
+  
+  # transit stops
+  el <- st_read("https://opendata.arcgis.com/datasets/8c6e2575c8ad46eb887e6bb35825e1a6_0.geojson") %>% 
+    st_transform('ESRI:102728')
+  bsl <- st_read("https://opendata.arcgis.com/datasets/2e9037fd5bef406488ffe5bb67d21312_0.geojson") %>% 
+    st_transform('ESRI:102728')
+  
+  septaStops <-
+    rbind(
+      el %>%
+        mutate(Line = "El") %>%
+        dplyr::select(Station, Line),
+      bsl %>%
+        mutate(Line ="Broad_St") %>%
+        dplyr::select(Station, Line)) %>%
+    st_transform('ESRI:102728') %>%
+    mutate(Legend = "Subway Stops") %>%
+    dplyr::select(Legend)
+  
+  # proximity to CBD using city_hall as proxy
+  city_hall <- bsl %>%
+    st_transform('ESRI:102728') %>%
+    filter(Station=="City Hall") %>%
+    mutate(Legend = "City Hall") %>%
+    dplyr::select(Legend)
+  
+  # schools
+  schools <- st_read('https://opendata.arcgis.com/datasets/d46a7e59e2c246c891fbee778759717e_0.geojson') %>% 
+    st_transform('ESRI:102728') %>% 
+    mutate(Legend = "Schools") %>% 
+    dplyr::select(Legend)
+  
+  
 }
 
 # Data Exploration
 {
-  # making simple tables of types of permits
-  permit_type <- sort(unique(dat_permit$permittype))
-  work_type <- sort(unique(dat_permit$typeofwork))
   
-  tow_tokeep <- work_type[grep("NEW|MAJOR",unique(work_type))] # typeofwork values to keep: values that contain "new", "major"
-  
-  dat_permit_tokeep <- dat_permit %>% 
-    mutate(year = format(mostrecentinsp, format="%Y")) %>% 
-    filter(typeofwork %in% tow_tokeep)
-  
-  dat_permit_complete <- dat_permit_tokeep %>% 
-    filter(status == "COMPLETED")  # only keep completed permits for now
   
   # predictors for permit count model
   # - inflation rate, unemployment rate,
