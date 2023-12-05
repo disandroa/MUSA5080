c577ebd0fe9fc45bfdf8f09465955e9f46784176
akidi <akira.disandro@pennmedicine.upenn.edu>
2023-12-04 19:43:50 -0500

    updated draft

--- Assignments/HW07-Final/aki_draft.R
+++ Assignments/HW07-Final/aki_draft.R
@@ -28,7 +28,15 @@ setwd("~/Documents/MUSA5080")
   # reading geojson file
   dat_permit <- st_read("https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+permits&filename=permits&format=geojson&skipfields=cartodb_id")
   
-  st_write(dat_permit,"Assignments/HW07-Final/data/permit_data.geojson")
+  # deecided to remove because it takes a long timee anyways + this exceeds the maximum for amount of space allowed on github
+  # st_write(dat_permit,"Assignments/HW07-Final/data/permit_data.geojson")
+  
+  # OPA housing permit data
+  
+  
+  # census data
+  census_vars <- c() # census variables of interest
+  
   
 }
 
@@ -38,7 +46,19 @@ setwd("~/Documents/MUSA5080")
   permit_type <- sort(unique(dat_permit$permittype))
   work_type <- sort(unique(dat_permit$typeofwork))
   
+  tow_tokeep <- work_type[grep("NEW|MAJOR",unique(work_type))] # typeofwork values to keep: values that contain "new", "major"
   
+  dat_permit_tokeep <- dat_permit %>% 
+    mutate(year = format(mostrecentinsp, format="%Y")) %>% 
+    filter(typeofwork %in% tow_tokeep)
+  
+  dat_permit_complete <- dat_permit_tokeep %>% 
+    filter(status == "COMPLETED")  # only keep completed permits for now
+  
+  # predictors for permit count model
+  # - inflation rate, unemployment rate,
+  # - demographics of neighborhood
+  # - building safety codes
 }
 
 
